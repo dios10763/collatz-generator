795e510cb3c21fe4e2a42642f0c023449add05b6
dios10763 <hjosmar439@gmail.com>
2026-05-08 17:37:58 -0700

    Implementación de un validador simple en el generador de Collatz en C++

--- collatz.cpp
+++ collatz.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <cmath>
+#include <limits>
 int main (int argc, char *argv[]) {
   unsigned long long numero;
   unsigned long long cantidad = 1;
   std::cout << "Ingresa un numero entero: ";
-  std::cin >> numero;
+  while(!(std::cin >> numero)) {
+    std::cout << "Error, ese no es un numero valido, prueba con otro número: ";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
   if (numero < 1) {
     return 0;
   }
