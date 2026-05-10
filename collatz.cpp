5e22e5493c62ef766ee1737e99c25b2cbdd7e293
dios10763 <hjosmar439@gmail.com>
2026-05-10 12:17:46 -0700

    Añadida detección de entrada por pipe además de soporte para argumentos

--- collatz.cpp
+++ collatz.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <limits>
+#include <string>
+#include <unistd.h>
 int main (int argc, char *argv[]) {
   unsigned long long numero;
   unsigned long long cantidad = 1;
   const unsigned long long maximo = std::numeric_limits<unsigned long long>::max() ;
-  std::cout << "Ingresa un numero entero: ";
-  while(!(std::cin >> numero)) {
-    std::cout << "Error, ese no es un numero valido, prueba con otro número: ";
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  }
-  if (numero < 1) {
+  if (argc > 1) {
+    numero = std::stoi(argv[1]);
+  } else {
+      if (isatty(STDIN_FILENO)) {
+       std::cout << "Ingresa un numero entero: ";
+      }
+      while(!(std::cin >> numero)) {
+      std::cout << "Error, ese no es un numero valido, prueba con otro número: ";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+     }
+  
+    }
+   if (numero < 1) {
     return 0;
   }
   while (true) {
