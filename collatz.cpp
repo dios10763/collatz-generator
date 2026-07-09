f116a69415a333c892863212d8a952536a236b95
dios10763 <hjosmar439@gmail.com>
2026-07-09 10:16:31 -0700

    refactor: modularizar collatz en funciones separadas

--- collatz.cpp
+++ collatz.cpp
@@ -1,62 +1,78 @@
 #include <iostream>
 #include <limits>
+#include <stdexcept>
 #include <string>
 #include <unistd.h>
-int main (int argc, char *argv[]) {
-  unsigned long long numero {};
-  unsigned long long cantidad {1};
-  const unsigned long long maximo = std::numeric_limits<unsigned long long>::max() ;
+bool tomar_argumento(int argc, char **argv, unsigned long long *ptr_numero) {
   if (argc > 1) {
     try {
-      numero = std::stoull(argv[1]);
-    }
-    catch (const std::invalid_argument&) {
+      *ptr_numero = std::stoull(argv[1]);
+      return true;
+    } catch (const std::invalid_argument &) {
       std::cerr << "Argumento invalido";
-      return 1;
+      return false;
+    } catch (const std::out_of_range &) {
+      std::cerr << "Numero fuera de rango";
+      return false;
     }
-    catch (const std::out_of_range&) {
-      std::cerr << "Número fuera de rango";
-      return 1;
-    };
-  } else {
-      if (isatty(STDIN_FILENO)) {
-       std::cout << "Ingresa un numero entero: ";
-      }
-      while(!(std::cin >> numero)) {
-        if (std::cin.eof()) {
-          std::cerr << "No se recibio un numero valido\n";
-                return 1;
-        }
-        std::cerr << "Error, ese no es un numero valido, prueba con otro número: ";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-     }
-  
-    }
-   if (numero < 1) {
-    return 0;
   }
+  return false;
+}
+bool tomar_input(unsigned long long *ptr_numero) {
+  if (isatty(STDIN_FILENO)) {
+    std::cout << "Ingresa un numero entero";
+  }
+  while (!(std::cin >> *ptr_numero)) {
+    if (std::cin.eof()) {
+      std::cerr << "No se recibio un numero valido";
+      return false;
+    }
+    std::cerr << "Overflow detectado mientras se realizaba la operación de "
+                 "multiplicación\n";
+    std::cin.clear();
+    std::cin.ignore();
+  }
+  return true;
+}
+bool modificar_numero(unsigned long long *ptr_numero,
+                      unsigned long long *ptr_cantidad) {
+  const unsigned long long maximo =
+      std::numeric_limits<unsigned long long>::max();
   while (true) {
-    std::cout << cantidad << ".- " << numero << "\n";
-    if (numero == 1) break;
-    cantidad++;
-    if (numero % 2 == 0) {
-      numero /= 2;
+    std::cout << *ptr_cantidad << ".- " << *ptr_numero << "\n";
+    if (*ptr_numero == 1) {
+      return true;
     }
-    else {
-      if (numero > maximo / 3) {
-        std::cerr << "Overflow detectado mientras se realizaba la operación de multiplicación\n";
-        return 1;
+    (*ptr_cantidad)++;
+    if (*ptr_numero % 2 == 0) {
+      *ptr_numero /= 2;
+    } else {
+      if (*ptr_numero > maximo / 3) {
+        std::cerr << "Overflow detectado mientras se realizaba la operación de "
+                     "multiplicación\n";
+        return false;
       }
-
-      unsigned long long temp = numero * 3;
-
-      if (temp > maximo -1) {
-        std::cerr << "Overflow detectado mientras se realizaba la operación de suma\n";
-        return 1;
+      unsigned long long temp = *ptr_numero * 3;
+      if (temp > maximo - 1) {
+        std::cerr << "Overflow detectado mientras se realizaba la operación de "
+                     "suma\n";
+        return false;
       }
-      numero = temp + 1;
+      *ptr_numero = temp + 1;
     }
+  }
+  return true;
+}
+int main(int argc, char *argv[]) {
+  unsigned long long numero{}, cantidad{1};
+  unsigned long long *ptr_numero = &numero;
+  unsigned long long *ptr_cantidad = &cantidad;
+  if (tomar_argumento(argc, argv, ptr_numero)) {
+    modificar_numero(ptr_numero, ptr_cantidad);
+  } else if (tomar_input(ptr_numero)) {
+    modificar_numero(ptr_numero, ptr_cantidad);
+  } else {
+    return 1;
   }
   std::cout << "La cantidad de numeros fue: " << cantidad << "\n";
   return 0;
